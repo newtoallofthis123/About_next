a919ec188c5d918b31eef358b75e43e6341c8b82
Noob Science <noobscience123@gmail.com>
2023-03-30 17:47:27 +0530

    Add Analytic

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,13 @@ import '@/styles/social.scss'
 import '@/styles/blog.scss'
 import "@/styles/prism/prism.css"
 import "@/styles/home.scss"
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  )
 }
