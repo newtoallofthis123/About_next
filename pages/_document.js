08290272ad454683e2cabf403f31057536f75a4d
Noob Science <noobscience123@gmail.com>
2023-03-30 13:05:47 +0530

    Migrate to vercel

--- pages/_document.js
+++ pages/_document.js
@@ -5,24 +5,28 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#f1fa00" />
+        <meta name="theme-color" content="#0f0f28" />
         <meta name="title" content="NoobScience"/>
         <meta name="description" content="Hi! I am Ishan. I am a Student and Open Source Enthusiast. This is my personal portfolio website. I Like to Code Stuff. Check out my work!"/>
         <meta name="title" content="NoobScience"/>
-        <meta name="keywords" content="noobscience, NoobScience, newtoallofthis123, technology, website"/>
+        <meta name="keywords" content="noobscience, noob science, NoobScience, newtoallofthis123, technology, website"/>
         <meta name="robots" content="index, follow, archive"/>
         <meta name="language" content="English"/>
         <meta name="author" content="NoobScience"/>
-
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://noobscience.rocks" />
+        <meta property="twitter:title" content="NoobScience" />
+        <meta property="twitter:description" content="Student and Open Source Enthusiast. I Like to Code Stuff. Check out my work!" />
+        <meta property="twitter:image" content="https://noobscience.rocks/assets/banner.png" />
         <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://new.noobscience.rocks/" />
+        <meta property="og:url" content="https://noobscience.rocks/" />
         <meta property="og:title" content="NoobScience"/>
         <meta property="og:description" content="Student and Open Source Enthusiast. I Like to Code Stuff. Check out my work!"/>
-        <meta property="og:image" content="https://new.noobscience.rocks/assets/banner.png"/>
+        <meta property="og:image" content="https://noobscience.rocks/assets/banner.png"/>
         <link rel="apple-touch-icon" href="/logo192.png" />
-        <link rel="canonical" href="https://new.noobscience.rocks" />
+        <link rel="canonical" href="https://noobscience.rocks" />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="mask-icon" href="/favicon.ico" color="#f1fa00" />
+        <link rel="mask-icon" href="/favicon.ico" color="#0f0f28" />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
