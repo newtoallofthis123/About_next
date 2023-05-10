8181e58b324bda71245a509e331377dbca2419fe
Noob Science <noobscience123@gmail.com>
2023-05-10 17:40:19 +0530

    Add Spotify Page

---
+++ pages/listen.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Seo } from '@components/seo'
+import Layout from '@components/layout'
+
+type Props = {}
+
+export default function Music({}: Props) {
+  return (
+      <Layout>
+          <Seo></Seo>
+          <div className="gap normalize">
+              <div className="page-div">
+                  <h1>Listen to Noob's Music</h1>
+                  <p>
+                      Listen to my curated playlist of music that I like. You
+                      are free to listen to it, but I don't guarantee that you
+                      will like it.
+                  </p>
+                  <iframe
+                      style={{ borderRadius: '12px' }}
+                      src="https://open.spotify.com/embed/playlist/11p4GVDQHOQk2R0ONtZEQl?si=807512e9b6354952"
+                      width="100%"
+                      height={352}
+                      frameBorder={0}
+                      allowFullScreen={true}
+                      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                      loading="lazy"
+                  ></iframe>
+              </div>
+          </div>
+      </Layout>
+  );
+}
